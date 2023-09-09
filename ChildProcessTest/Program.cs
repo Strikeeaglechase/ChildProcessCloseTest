6f1a2a8c86057dca18c4a0eaa59a329b3d7a41c6
Strikeeaglechase <Strikeeaglechase@gmail.com>
2023-09-09 19:32:25 -0400

    Created project

---
+++ ChildProcessTest/Program.cs
@@ -0,0 +1,10 @@
+﻿StreamWriter writer = File.CreateText("./child.log");
+writer.AutoFlush = true;
+while (true)
+{
+    Thread.Sleep(1000);
+    var time = DateTime.Now;
+    var log = $"It is currently {time:s}";
+    Console.WriteLine(log);
+    writer.WriteLine(log);
+}
---
+++ dist/index.js
@@ -0,0 +1,26 @@
+import { execFile } from "child_process";
+import fs from "fs";
+const pathToChildOpts = ["../ChildProcessTest/bin/Debug/net6.0/ChildProcessTest.exe", "./ChildProcessTest/bin/Debug/net6.0/ChildProcessTest.exe"];
+let pathToUse = "";
+for (const pathToChild of pathToChildOpts) {
+    if (fs.existsSync(pathToChild)) {
+        pathToUse = pathToChild;
+        break;
+    }
+}
+if (!pathToUse) {
+    console.log(`No child process found, please build first`);
+    process.exit(1);
+}
+const outLog = fs.createWriteStream("./parent.log", { flags: "a" });
+const log = (message) => {
+    console.log(message);
+    outLog.write(`${message}\n`);
+};
+log(`Starting`);
+const child = execFile(pathToUse);
+child.stdout.on("data", (data) => log(`Child stdout: ${data.toString().trim()}`));
+child.stderr.on("data", (data) => log(`Child stderr: ${data.toString().trim()}`));
+child.on("error", (error) => log(`Child error: ${error}`));
+child.on("close", (code) => log(`Child process exited with code ${code}. Why did this happen?`));
+child.on("exit", (code) => log(`Child process exited with code ${code}.`));
